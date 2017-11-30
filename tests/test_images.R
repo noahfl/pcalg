a63e21ebb5c052a9d3b4d6d9f055129738bde4a1
noahfl <blu3flannel@gmail.com>
2017-11-30 13:08:16 -0500

    added double simulation test

--- tests/test_images.R
+++ tests/test_images.R
@@ -2,15 +2,28 @@
 data(gmG)
 
 ## Define the score (BIC)
-score <- new("GaussL0penObsScore", gmG8$x, lambda=2)
+score <- new("GaussL0penObsScore", gmG8$x)#, lambda=2)
 
-print(log(nrow(gmG8$x)))
+
+#set.seed(40)
+set.seed(50)
+p <- 8
+n <- 5000
+## true DAG:
+vars <- c("Author", "Bar", "Ctrl", "Goal", paste0("V",5:8))
+gGtrue <- randomDAG(p, prob = 0.3, V = vars)
+gmA  <- list(x = rmvDAG(n, gGtrue, back.compatible=TRUE), g = gGtrue)
+gmA8 <- list(x = rmvDAG(n, gGtrue),                       g = gGtrue)
+
+score_alt <- new("GaussL0penObsScore", gmA8$x)#, lambda=2)
+
+#print(log(nrow(gmG8$x)))
 
 ## Estimate the essential graph
-images <- new("IMaGES", scores = list(score), penalty=0)
+images <- new("IMaGES", scores = list(score, score_alt), penalty=0)
 
 images.fit <- images$results[[1]]
-
+images2.fit <- images$results[[2]]
 print(images.fit)
 
 
@@ -25,6 +38,15 @@ if (require(Rgraphviz)) {
 } else { ## alternative:
   str(images.fit, max=2)
   as(as(images.fit$essgraph,"graphNEL"),"Matrix")
+}
+
+if (require(Rgraphviz)) {
+  par(mfrow=c(1,2))
+  plot(images2.fit[[2]], main = "Estimated CPDAG")
+  plot(gmA8$g, main = "True DAG")
+} else { ## alternative:
+  str(images2.fit, max=2)
+  as(as(images2.fit$essgraph,"graphNEL"),"Matrix")
 }
 
 score2 <- new("GaussL0penObsScore", gmG8$x, lambda=2)
