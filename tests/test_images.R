f0bc77345c9dc61606c96774b2265460b270b1a0
Noah Frazier-Logue <blu3flannel@gmail.com>
2017-12-15 13:19:26 -0500

    working on scoring

--- tests/test_images.R
+++ tests/test_images.R
@@ -77,7 +77,7 @@ run_originals <- function(datasets) {
   fits = list()
   for (i in 1:length(datasets)) {
     images <- new("IMaGES", scores = list(datasets[[i]]), penalty=0)
-    fits[[i]] <- images$results[[1]]
+    fits[[i]] <- images$results
   }
   return(fits)
 }
@@ -154,19 +154,20 @@ driver <- function() {
 #   as(as(images2.fit$essgraph,"graphNEL"),"Matrix")
 # }
 
-data(gmG)
+# data(gmG)
+# 
+# score2 <- new("GaussL0penObsScore", gmG8$x, lambda=2)
+# 
+# ges.fit <- ges(score2)
+# 
+# ## Plot the estimated essential graph and the true DAG
+# if (require(Rgraphviz)) {
+#   par(mfrow=c(1,2))
+#   plot(ges.fit[[2]], main = "Estimated CPDAG")
+#   plot(gmG8$g, main = "True DAG")
+# } else { ## alternative:
+#   str(ges.fit, max=2)
+#   as(as(ges.fit$essgraph,"graphNEL"),"Matrix")
+# }
 
-score2 <- new("GaussL0penObsScore", gmG8$x, lambda=2)
-
-ges.fit <- ges(score2)
-
-## Plot the estimated essential graph and the true DAG
-if (require(Rgraphviz)) {
-  par(mfrow=c(1,2))
-  plot(ges.fit[[2]], main = "Estimated CPDAG")
-  plot(gmG8$g, main = "True DAG")
-} else { ## alternative:
-  str(ges.fit, max=2)
-  as(as(ges.fit$essgraph,"graphNEL"),"Matrix")
-}
-
+driver()
